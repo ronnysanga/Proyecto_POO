466b1cf77690f61f3aaf2f85e3af955f7b972734
esguzman <esguzman@espol.edu.ec>
2021-11-28 22:36:08 -0500

    cambio elayas

--- Proyecto/src/main/java/ec/edu/espol/proyecto/Main.java
+++ Proyecto/src/main/java/ec/edu/espol/proyecto/Main.java
@@ -16,7 +16,8 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("proyect");
+        System.out.println("algo mas claro");
+        System.out.println("hola sam ");
     }
     
 }
