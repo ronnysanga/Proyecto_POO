5faff77e4ad83e5d7cdacf5f33ff9d369eaff00a
esguzman <esguzman@espol.edu.ec>
2021-11-30 22:50:11 -0500

    cambioMiembroJurado paso1Elias

--- Proyecto/src/main/java/ec/edu/espol/model/MiembroJurado.java
+++ Proyecto/src/main/java/ec/edu/espol/model/MiembroJurado.java
@@ -6,13 +6,48 @@
 package ec.edu.espol.model;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
  * @author dell
  */
-public class MiembroJurado {
+public class MiembroJurado extends Persona {
     private String perfil;
     private ArrayList<Evaluacion> evaluaciones;
+    
+    /**
+     *
+     * @param perfil
+     */
+    public MiembroJurado(String perfil) {
+        this.perfil = perfil;
+        
+    }
+    public static String nextMiembroJurado (Scanner scan){
+        System.out.println("Ingrese su perfil: ");
+          
+        String perfil = scan.next();
+        return perfil;  
+           
+    }
+    
+    //public void
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public ArrayList<Evaluacion> getEvaluaciones() {
+        return evaluaciones;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+
+    public void setEvaluaciones(ArrayList<Evaluacion> evaluaciones) {
+        this.evaluaciones = evaluaciones;
+    }
     
 }
