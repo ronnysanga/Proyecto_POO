83c0a89f3535c82770de82a19e9d6141e016437e
ronnysanga <rsanga@espol.edu.ec>
2021-11-30 22:45:20 -0500

    Modificaciones clase Dueño y Persona Ronny

--- Proyecto/src/main/java/ec/edu/espol/model/Dueño.java
+++ Proyecto/src/main/java/ec/edu/espol/model/Dueño.java
@@ -5,6 +5,9 @@
  */
 package ec.edu.espol.model;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 import java.util.ArrayList;
@@ -14,30 +17,65 @@ import java.util.ArrayList;
  *
  * @author dell
  */
-public class Dueño {
+public class Dueño extends Persona{
     private int id;
-    private String nombres;
-    private String apellidos;
-    private String email;
     private String direccion;
     private ArrayList<Mascota> mascotas;
 
-    public Dueño(String nombres, String apellidos, String email) {
-        this.nombres = nombres;
-        this.apellidos = apellidos;
-        this.email = email;
+
+    public Dueño(int id, String direccion, String nombres, String apellidos, String telefono, String email) {
+        super(nombres, apellidos, telefono, email);
+        this.id = id;
+        this.direccion = direccion;
+    }
+
+    public void saveFile(String nomfile){
+        try(PrintWriter pw = new PrintWriter(new FileOutputStream(new File(nomfile),true)))
+        {
+            //datosd1|mas1|mas2
+            pw.println(this.id+"|"+this.direccion+"|"+this.nombres+"|"+this.apellidos+"|"+this.telefono+"|"+this.email); 
+            //for mascotas y append           
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
     
+    //Corregir readfromfile
+    public static ArrayList<Dueño> readfile(String nomfile){
+        ArrayList<Dueño> dueños = new ArrayList<>();
+        try(Scanner sc = new Scanner(new File(nomfile))){
+            while(sc.hasNextLine()){
+            String linea = sc.nextLine();
+            String[] tokens = linea.split("|");
+            Dueño d = new Dueño(Integer.parseInt(tokens[0]), tokens[1], tokens[2], tokens[3], tokens[4], tokens[5]);
+            dueños.add(d);
+            }//se podia agregar a los dueños que habia     
+            return dueños;
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }        
+        return dueños;
+    }
     
-    public static Dueño nextDueño(Scanner ekisde){
-        System.out.println("Ingrese sus nombres: ");
-            String nom = ekisde.next();
-        System.out.println("Ingrese sus apellidos: ");
-            String ap = ekisde.next();
+    public static Dueño nextDueño(Scanner sc){
+        //this.id+"|"+this.direccion+"|"+this.nombres+"|"+this.apellidos+"|"+this.telefono+"|"+this.email
+        System.out.println("Ingrese id:");
+        int id = sc.nextInt();
+        System.out.println("Ingrese direccion: ");
+        String direc = sc.next();
+        System.out.println("Ingrese nombre: ");
+        String nom = sc.next();
+        System.out.println("Ingrese apellido: ");
+        String ape = sc.next();
+        System.out.println("Ingrese telefono: ");
+        String phone = sc.next();
         System.out.println("Ingrese su email: ");
-            String em = ekisde.next();
-        
-            return new Dueño(nom,ap,em);
+        String ema = sc.next();
+                
+        Dueño dueño = new Dueño(id ,direc ,nom ,ape ,phone ,ema);   
+        return dueño;
         
     }
 
@@ -45,8 +83,7 @@ public class Dueño {
     @Override
     
     public String toString() {
-        return "Due\u00f1o{" + "id=" + id + ", nombres=" + nombres + ", apellidos=" + apellidos + ", email=" + email + '}';
+        return String.valueOf(this.id)+"-"+this.direccion +"-"+ this.nombres +"-"+ this.apellidos +"-"+this.telefono+"-"+this.email;
     }
 
-    
 }
--- Proyecto/src/main/java/ec/edu/espol/model/Persona.java
+++ Proyecto/src/main/java/ec/edu/espol/model/Persona.java
@@ -9,10 +9,35 @@ package ec.edu.espol.model;
  *
  * @author dell
  */
-public class Persona {
+public class Persona {   
     protected String nombres;
     protected String apellidos;
     protected String telefono;
     protected String email;
+
+    public Persona(String nombres, String apellidos, String telefono, String email) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+    
     
 }
