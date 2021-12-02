d5ef8f1d06a1f7e0fdf07aa657d87342efa210a1
Samsvill <samantha.sharid@gmail.com>
2021-12-02 15:05:09 -0500

    Cambio Samantha

--- Proyecto/src/main/java/ec/edu/espol/model/Mascota.java
+++ Proyecto/src/main/java/ec/edu/espol/model/Mascota.java
@@ -5,8 +5,12 @@
  */
 package ec.edu.espol.model;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Scanner;
 
 /**
  *
@@ -21,4 +25,37 @@ public class Mascota {
     private Dueño dueño;
     private ArrayList<Inscripcion> inscripciones;
     
+    public Mascota(int idDueño, String nombre, String raza, String tipo, Date fechanacimiento) {
+        this.idDueño = idDueño;
+        this.nombre = nombre;
+        this.raza = raza;
+        this.tipo = tipo;
+        this.fechanacimiento = fechanacimiento;
+        
+    }
+    
+    public static Object nextMascota(Scanner mas){
+        System.out.println("ID del dueño: ");
+            int idDueño = mas.nextInt();
+        System.out.println("Ingrese nombre de la mascota: ");
+            String nombre = mas.nextLine();
+        System.out.println("Ingrese el tipo de animal: ");
+            String tipo = mas.next();
+        System.out.println("Ingrese la raza de animal: ");
+            String raza = mas.nextLine();
+        System.out.println("Ingrese la fecha de nacimiento de la mascota: ");
+            String fecha = mas.next();
+        
+            return new Mascota(idDueño,nombre,tipo,raza,fecha);
+    }
+    
+    public void saveFile(String nomfile){
+        try(PrintWriter pw = new PrintWriter(new FileOutputStream(new File(nomfile),true)))
+        {
+            pw.println(this.idDueño+"|"+this.nombre+"|"+this.raza+"|"+this.tipo+"|"+this.fechanacimiento);        
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
